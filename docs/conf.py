0fbe69476d9999d778952af7743f840c41ed32a9
Shashank Kapadia <kapadia.s@hotmail.com>
2015-08-23 20:55:34 -0400

    ---

--- docs/conf.py
+++ docs/conf.py
@@ -65,8 +65,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ValidationLib'
-copyright = u'2015, Author'
-author = u'Author'
+copyright = u'2015 AIR Worldwide, Shashank Kapadia'
+author = u'Shashank Kapadia'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
